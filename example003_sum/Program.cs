115644f8ffc58d5154b29acfd0df2178cf82d1bc
«Arsen» <rchi1504@gmail.com>
2022-04-09 20:29:56 +0300

    random

--- example003_sum/Program.cs
+++ example003_sum/Program.cs
@@ -1,5 +1,6 @@
-﻿int numberA = 33;
-int numberB = 4;
-int result = numberA - numberB;
-Console.WriteLine(numberA + numberB);
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
 Console.WriteLine(result);
